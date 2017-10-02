c1a0871700d7c6d682c3d6694cdd1ee3b5c9a9da
Moses <mosesflava@gmail.com>
2017-10-02 20:07:56 +0100

    Wrapped cube up with our texture. Will automate cubes soon

--- GLProjects/GLApp/GLApp/Shaders/frag.frag
+++ GLProjects/GLApp/GLApp/Shaders/frag.frag
@@ -1,5 +1,4 @@
 #version 330 core
-in vec3 ourColour;
 in vec2 TexCoord;
 
 out vec4 color;
--- GLProjects/GLApp/GLApp/main.cpp
+++ GLProjects/GLApp/GLApp/main.cpp
@@ -9,22 +9,14 @@
 
 const GLint width = 600, height = 800;
 
+void  GLFWInit();
+
 
 
 int main()
 {
-	glfwInit();
 
-	//Setting the version of our glfw window. Different versions have things like immediate mode removed ( 3.3)
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	
-	//Doesnt care for backwards compatibility
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//Cares for forwards
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	//Toggles Resizeability
-	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-
+	GLFWInit();
 
 	//Creating window
 	
@@ -59,54 +51,80 @@ int main()
 	glViewport(0, 0, screenWidth, screenHeight);
 
 	glEnable(GL_BLEND);
+	glEnable(GL_DEPTH_TEST);
 	glBlendFunc(GL_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	Shader ourShader("Shaders/core.vs", "Shaders/frag.frag");
-	//Attachment process
-	GLfloat verts[] =
-	{
-		//positon						//colour				//TexCood(Normalised)
-		0.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f,				1.0f, 1.0f, //TopRight
-		0.5f, -0.5f, 0.0f,		0.0f, 1.0f,	0.0f,				1.0f, 0.0f,	//BottomRight
-		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,				0.0f, 0.0f, //BottomLeft
-		- 0.5f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f,				0.0f, 1.0f //TopLeft
 
 
-	};
-	GLuint indice[] =
-	{
-		//Indexing shared vertexs
-		0, 1, 3, //First Triangle
-		1, 2, 3, //Second Triangle
+
+	// use with Perspective Projection
+	GLfloat vertices[] = {
+		//xyz tex coord
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 
 	//Generate our buffers and array
-
-	GLuint vbo, vao, ebo;
+	GLuint vbo, vao;
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
+
 
 	//Bind the vao first, then bind and set the buffers / attrib pointers
 	glBindVertexArray(vao);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indice), indice, GL_STATIC_DRAW);
 
 	//Position Attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	//Colour attributes
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
 
 	//TexCoord attributes
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
 
 	//Unbind vao
@@ -126,7 +144,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	//Load image
-	unsigned char * image = SOIL_load_image("Images/Spiral.png", &texWidth, &texHeight, 0, SOIL_LOAD_RGBA);
+	unsigned char * image = SOIL_load_image("Images/Brick.jpg", &texWidth, &texHeight, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	//Gen mipmaps (LODING)
 	glGenerateMipmap(GL_TEXTURE_2D);
@@ -135,47 +153,80 @@ int main()
 	SOIL_free_image_data(image);
 	//Unbinding it now
 	glBindTexture(GL_TEXTURE_2D, 0);
+	//perspetive projection
+	glm::mat4 projection;
+
+	//view frustrum = aspect ratio (screenwidth / screen height), near clipping plane anything closer than the number isnt rendered same with far clipping plane
+	//perspective
+	projection = glm::perspective(45.0f, (GLfloat)screenWidth / screenHeight, 0.1f, 1000.0f);
+
+	//ortho
+	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
-		//check for events/inputs
+																	//check for events/inputs
 		glfwPollEvents();
 		
+																	
 		//GAME LOGIC
 
-		//RENDER
+																	//RENDER
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//DRAW
 
-		//Using our shader
-		ourShader.use();
-
-		//Create transform
-		glm::mat4 transform;
-		//Translate transform 
-		transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
-		//Rotate it along z axis( not using it currently so nothing should change) by tick
-		transform = glm::rotate(transform, (GLfloat)glfwGetTime() * -2.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-
-		GLint transformLoc = glGetUniformLocation(ourShader.shaderProgram, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-
+																	//DRAW
 		//Activating the texture
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glUniform1i(glGetUniformLocation(ourShader.shaderProgram, "ourTexture"), 0);
 
+		//Using our shader
+		ourShader.use();
+		
+		glm::mat4 model;
+		glm::mat4 view;
+		//perspective
+		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+
+		//Ortho
+		//view = glm::translate(view, glm::vec3(screenWidth / 2.0f, screenHeight / 2.0f, -700.0f));
+		//model = glm::rotate(model, (GLfloat) 0.5f, glm::vec3(1.0f, 0.0f, 0.0f));
+
+		GLint modelLoc = glGetUniformLocation(ourShader.shaderProgram, "model");
+		GLint viewLoc = glGetUniformLocation(ourShader.shaderProgram, "view");
+		GLint projectionLoc = glGetUniformLocation(ourShader.shaderProgram, "projection");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(vao);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
+
+		//swap screen buffers
 		glfwSwapBuffers(window);
 
 	}
 	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(1, &vbo);
-	glDeleteBuffers(1, &ebo);
 	return EXIT_SUCCESS;
+}
+
+void  GLFWInit()
+{
+	glfwInit();
+
+	//Setting the version of our glfw window. Different versions have things like immediate mode removed ( 3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Doesnt care for backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Cares for forwards
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//Toggles Resizeability
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 }
