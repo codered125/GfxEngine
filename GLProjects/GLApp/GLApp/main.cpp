9c821786f1ab9c94f948b68229867e0faf4450fb
Moses <mosesflava@gmail.com>
2017-10-10 08:38:51 +0100

    switch BG TO black for clarity

--- GLProjects/GLApp/GLApp/main.cpp
+++ GLProjects/GLApp/GLApp/main.cpp
@@ -209,7 +209,7 @@ int main()
 		//GAME LOGIC
 
 																	//RENDER
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.0, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//Using our shader
