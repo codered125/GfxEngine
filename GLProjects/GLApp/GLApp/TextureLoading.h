1fde71d0d1d2b24548c36d069bda0b98e452b255
Moses <mosesflava@gmail.com>
2017-12-13 15:15:39 +0000

    still delving into shadow mapping

--- GLProjects/GLApp/GLApp/TextureLoading.h
+++ GLProjects/GLApp/GLApp/TextureLoading.h
@@ -36,24 +36,22 @@ public:
 		return textureID;
 	}
 
-	static void setShadowMap()
+	static void SetupDPMapTex( GLuint * depthMapFBO, GLuint *depthMap)
 	{
 
 		const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
-		GLuint depthMapFBO, depthMap;
+		glGenFramebuffers(1, depthMapFBO);
 
-		glGenFramebuffers(1, &depthMapFBO);
-
-		glGenTextures(1, &depthMap);
-		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glGenTextures(1, depthMap);
+		glBindTexture(GL_TEXTURE_2D, *depthMap);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+		glBindFramebuffer(GL_FRAMEBUFFER, *depthMapFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, *depthMap, 0);
 		glDrawBuffer(GL_NONE);
 		glReadBuffer(GL_NONE);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
