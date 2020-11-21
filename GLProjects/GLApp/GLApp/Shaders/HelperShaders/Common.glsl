ba4977071f4b105e91f4719bc18c1b09066017af
Moses <mosesflava@gmail.com>
2020-11-21 18:28:00 +0000

    Pulling out pbr implementation from forward pass code
    Made the loop more programmable

---
+++ GLProjects/GLApp/GLApp/Shaders/HelperShaders/Common.glsl
@@ -0,0 +1,56 @@
+#ifndef COMMON
+#define COMMON
+//-------------------------------------------------------------------
+
+struct DirLight 
+{
+	float intensity;
+	vec3 position;
+	vec3 direction;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+//-------------------------------------------------------------------
+
+struct PointLight 
+{
+    float constant;
+    float linear;
+	float intensity;
+    float quadratic;
+
+	vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+//-------------------------------------------------------------------
+
+struct Material
+{
+    sampler2D  texture_diffuse;
+    sampler2D  texture_specular;
+	sampler2D  texture_normal;
+	sampler2D  texture_height;
+	sampler2D  texture_roughness;
+	sampler2D  texture_metallic;
+	sampler2D  texture_ao;
+	float shininess;
+};
+
+//-------------------------------------------------------------------
+
+struct LinearMatVals
+{
+	float roughness;
+	float metallic;
+    float ao;
+	vec3 diffuse;
+};
+#endif
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
